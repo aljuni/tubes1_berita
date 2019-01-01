86af85b6d63faae66e9bf4f9cbd8b108c32ccb53
aljuni <aljunihirossie01@gmail.com>
2019-01-02 02:05:47 +0700

    inisialisasi

---
+++ home.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <title>3 Col Portfolio - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/3-col-portfolio.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="logooo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="home.php?konten=about">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="home.php?konten=disclaimer">Disclaimer</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="home.php?konten=tos">Term Of Use</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <br><br><br><br>
+  
+<?php   include 'pusat.php'; ?>
+
+<footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
---
+++ pusat.php
@@ -0,0 +1,74 @@
+<?php   
+  if ($_GET['konten'] == 'about') {
+  echo '<div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-12">
+          <br>
+          <div align="center"><h1>TENTANG KAMI</h1></div><br> 
+          <p align="justify">Nama website ini diambil dari nama asli sang pemilik, yaitu Aljuni Hirossie. Aljuni adalah seorang mahasiswa di salah satu kampus yang ada di daerah Indramayu. Dia senang menulis, terutama tentang berita viral dan juga teknologi.<br>Terimakasih sudah sudi mampir di blog sederhana ini, jangan lupa berkunjung kembali yah :D</p>
+
+   
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+       
+
+          <!-- Side Widget -->
+          
+      <!-- /.row -->
+
+    </div><br><br><br>';
+                                           
+         
+  }elseif ($_GET['konten']=='disclaimer') {
+    echo '<div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-12">
+          <br>
+          <div align="center"><h1>DISCLAIMER</h1></div> 
+          <div align="center">
+            <img src="disclaimer.jpg">
+          </div>
+
+          
+          <p align="justify">Semua isi dari blog ini insyaallah adalah hasil dari tulisan penulis. Adapun jika ada materi didalam blog ini yang mungkin ada unsur duplikasi baik berupa teks maupun gambar. Sungguh penulis tidak ada niat untuk melanggar hak cipta. Jika anda adalah pemilik sah dari salah satu gambar atau artikel di blog ini dan anda berkeinginan untuk tidak ingin ditampilkan, maka silahkan hubungi kami. Insyaallah kami akan segera melakukan yang diperlukan, baik untuk menghapus maupun memberikan credit/link dimana gambar atau artikel tersebut berada.</p>
+
+   
+
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+         </div>
+         <br><br><br></div>';
+  }else{
+    echo '<div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-12">
+          <br>
+          <div align="center"><h1>TERM OF USE</h1></div> 
+          <div align="center">
+            <img src="tos.jpg"><br><br>
+          </div>
+          
+          <p align="justify">1. Konten redaksi yang ada pada blog ini sebagian besar menggunakan teknik otak berfikir dan tangan untuk mengetik. pembuatan blog ini pun didasari sabagai media belajar dan memanen informasi yang ada, tanpa ada sedikitpun niatan copas dari blog lainnya dan/ menyinggung pihak lain. Setiap orangpun harus memiliki referensi topik sebelum membuat artike bukan? Jika ada tulisan di blog ini yang secara tidak sengaja menyinggung pihak lain, silahkan berkomentar atau menghubungi kami, agar dapat segera di hapus atau di ganti. <br><br> 2. Kalian diperbolehkan mempublilasikan kembali sebagian maupun sepenuhnya tulisan di blog ini dengan atau tanpa persetujuan kami dengan catatan memberikan alamat credit pada tulisan tersebut(bisa berupa link aktif maupun teks url biasa). <br><br> 3. Karena kesibukan terntentu saya tidak bisa selalu memoderasi komentar, jadi saya membebaskan kalian berkomentar di blog saya dengan catatan, tidak berkomentar SARA, PORNOGRAFI, PROMOSI, SPAM, dan komentar yang merugikan blog, jika terdeteksi di atas akan di hapus. <br><br> 4. Saya memperbolehkan anda melakukan BlogWalking, say hello, pertamaxx, dsb. Namun saya tidak menjamin akan melakukan kunjungan balik dan, sebisa mungkin saya akan membalas setiap komentar yang masuk, tapi mohon maaf jika saya lama dalam membalas komntar karena saya tidak setiap hari di depan monitor...  :D  <br><br>Terimakasih sudah mengunjungi Blog ini jangan lupa untuk mengunjunginya kembali ^_^</p>
+
+   
+
+
+        </div>
+         </div></div>';
+  }
+?>
+ 
